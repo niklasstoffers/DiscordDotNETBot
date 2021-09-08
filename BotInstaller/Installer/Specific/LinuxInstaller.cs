224031116dd9ac7aef2dba4bfd595b87ac29f75e
Niklas Stoffers <stoffers.niklas@gmx.de>
2021-09-08 19:31:56 +0200

    Added Linux installer

---
+++ BotInstaller/Installer/Specific/LinuxInstaller.cs
@@ -0,0 +1,19 @@
+﻿using BotInstaller.SysArchitecture;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BotInstaller.Installer.Specific
+{
+    public class LinuxInstaller : InstallerBase
+    {
+        public LinuxInstaller(Logger logger) : base(logger, OS.Linux) { }
+
+        public override bool CheckPermission()
+        {
+            return Mono.Unix.Native.Syscall.getuid() == 0;
+        }
+    }
+}
