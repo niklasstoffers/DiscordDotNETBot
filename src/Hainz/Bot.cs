98ce5db96332482d3a785be0af5b903c6d3c83fe
Niklas Stoffers <stoffers.niklas@gmx.de>
2022-12-15 21:25:07 +0100

    Add simple NLog logging

--- src/Hainz/Bot.cs
+++ src/Hainz/Bot.cs
@@ -1,18 +1,26 @@
 using Microsoft.Extensions.Hosting;
+using Microsoft.Extensions.Logging;
 
 namespace Hainz;
 
 internal sealed class Bot : IHostedService
 {
+    private ILogger<Bot> _logger;
+
+    public Bot(ILogger<Bot> logger) 
+    {
+        _logger = logger;
+    }
+
     public Task StartAsync(CancellationToken cancellationToken)
     {
-        Console.WriteLine("Starting");
+        _logger.LogInformation("Starting");
         return Task.CompletedTask;
     }
 
     public Task StopAsync(CancellationToken cancellationToken)
     {
-        Console.WriteLine("Stopping");
+        _logger.LogInformation("Stopping");
         return Task.CompletedTask;
     }
 }
--- src/Hainz/Program.cs
+++ src/Hainz/Program.cs
@@ -5,16 +5,20 @@ using Hainz;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
+using Microsoft.Extensions.Logging;
+using NLog.Extensions.Logging;
 
 await Host.CreateDefaultBuilder(args)
     .UseServiceProviderFactory(new AutofacServiceProviderFactory())
     .ConfigureAppConfiguration(ConfigureApp)
     .ConfigureServices(ConfigureServices)
     .ConfigureContainer<ContainerBuilder>(ConfigureContainer)
+    .ConfigureLogging(ConfigureLogging)
     .RunConsoleAsync();
 
 void ConfigureApp(IConfigurationBuilder configBuilder)
 {
+    configBuilder.SetBasePath(AppDomain.CurrentDomain.BaseDirectory);
     configBuilder.AddJsonFile("appsettings.json", optional: true);
 }
 
@@ -27,4 +31,11 @@ void ConfigureContainer(ContainerBuilder builder)
 {
     var assembly = Assembly.GetExecutingAssembly();
     builder.RegisterAssemblyModules(assembly);
+}
+
+void ConfigureLogging(HostBuilderContext hostContext, ILoggingBuilder loggingBuilder) 
+{
+    loggingBuilder.ClearProviders();
+    loggingBuilder.SetMinimumLevel(LogLevel.Trace);
+    loggingBuilder.AddNLog("nlog.config");
 }
