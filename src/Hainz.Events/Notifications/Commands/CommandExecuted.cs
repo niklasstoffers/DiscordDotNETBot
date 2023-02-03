fef630d86efb6087148f2f612c39a0eb9b1d8f59
Niklas Stoffers <stoffers.niklas@gmx.de>
2023-02-03 11:42:24 +0100

    Make notifications records

--- src/Hainz.Events/Notifications/Commands/CommandExecuted.cs
+++ src/Hainz.Events/Notifications/Commands/CommandExecuted.cs
@@ -4,18 +4,4 @@ using MediatR;
 
 namespace Hainz.Events.Notifications.Commands;
 
-public class CommandExecuted : INotification
-{
-    public Optional<CommandInfo> CommandInfo { get; init; }
-    public ICommandContext CommandContext { get; init; }
-    public IResult Result { get; init; }
-
-    public CommandExecuted(Optional<CommandInfo> commandInfo,
-                           ICommandContext commandContext,
-                           IResult result)
-    {
-        CommandInfo = commandInfo;
-        CommandContext = commandContext;
-        Result = result;
-    }
-}
+public record CommandExecuted(Optional<CommandInfo> CommandInfo, ICommandContext CommandContext, IResult Result) : INotification;
--- src/Hainz.Events/Notifications/Messages/MessageReceived.cs
+++ src/Hainz.Events/Notifications/Messages/MessageReceived.cs
@@ -3,12 +3,4 @@ using MediatR;
 
 namespace Hainz.Events.Notifications.Messages;
 
-public class MessageReceived : INotification
-{
-    public SocketMessage Message { get; init; }
-
-    public MessageReceived(SocketMessage message)
-    {
-        Message = message;
-    }
-}
+public record MessageReceived(SocketMessage Message) : INotification;
